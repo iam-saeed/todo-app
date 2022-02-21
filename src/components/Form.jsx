b6f45a05fbed4e9e9a89e33f38d7fdd9c451b308
Saeed Khan <saeed@skdevelopment.org>
2022-02-21 13:02:54 -0700

    form component added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Form = () => {
+const Form = (props) => {
+    const [name, setName] = useState("");
+
+    const handleChange = (e) => {
+        setName(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        props.addTask(name)
+        setName("")
+    }
+    const disabled = name.length === 0 ? true : false;
   return (
-    <div>Form</div>
+    <form onSubmit={handleSubmit}>
+        <h2 className="label-wrapper">
+            <label htmlFor="new-todo-input">What needs to be done?</label>
+        </h2>
+        <input 
+        type="text"
+        id="new-todo-input"
+        value={name}
+        autocomplete="off"
+        onChange={handleChange}
+        />
+        <button disabled={disabled}>Add</button>
+    </form>
   )
 }
 
